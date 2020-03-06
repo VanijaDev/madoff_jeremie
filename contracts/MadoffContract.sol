170324c7779fe773d2b51a220706f8d7ba3069b5
Ivan Solomichev <vanijatko.social@gmail.com>
2020-03-06 16:21:32 +0200

    purchase function in process

---
+++ contracts/MadoffContract.sol
@@ -0,0 +1,67 @@
+pragma solidity 0.4.25;
+
+import "./SafeMath.sol";
+
+contract MadoffContract {
+  using SafeMath for uint256;
+
+  uint8 constant PURCHASE_PERCENT_JACKPOT = 40;
+  uint8 constant PURCHASE_PERCENT_PURCHASED_SHARES = 50;
+  uint8 constant PURCHASE_PERCENT_BERNARD_WEBSITE = 5;  //  both has 5%
+
+  uint8 constant JACKPOT_PERCENT_WINNER = 80;
+  uint8 constant JACKPOT_PERCENT_SHAREHOLDERS = 80;
+
+  uint8 ongoingStage;
+
+  // remove from constructor
+  // address constant OWNER_ADDR = "";
+  address OWNER_ADDR;
+
+  uint16[14] sharePriceForStage = [10, 20, 40, 80, 125, 160, 200, 250, 320, 500, 800, 1000, 1000, 1000];
+  uint16[14] maxBlocksForStage = [21600, 18000, 14400, 10800, 7200, 3600, 1200, 600, 300, 100, 20, 10, 7, 4];
+  uint32[14] maxSharesForStage = [2500, 7500, 10625, 23125, 33125, 95625, 158125, 558125, 948750, 2948750, 4511250, 14511250, 27011250, 52011250];
+
+  uint256 public ongoingJackpot;
+  uint256 public ongoingBernardFee;
+  mapping(address => uint256) public websiteFee;
+  mapping(address => uint256) public sharesForAddr;
+
+
+  constructor(address _OWNER_ADDR) public {
+      OWNER_ADDR = _OWNER_ADDR;
+  }
+
+  /**
+   * @dev Purchase share(s).
+   * @param _websiteAddr Website address, that trx was sent from.
+   */
+  function purchase(address _websiteAddr) public payable {
+    uint256 sharesNumber = getSharesNumberAndUpdateOngoingStage(msg.value);
+    require(sharesNumber > 0, "Min 1 share");
+
+    //  make calculations
+    uint256 partJackpot = msg.value.mul(uint256(PURCHASE_PERCENT_JACKPOT)).div(uint256(100));
+    ongoingJackpot = ongoingJackpot.add(partJackpot);
+
+    uint256 partPurchasedShares = msg.value.mul(uint256(PURCHASE_PERCENT_PURCHASED_SHARES)).div(uint256(100));
+    //  add to all shareholders
+
+    uint256 partBernardWebsiteFee = msg.value.mul(uint256(PURCHASE_PERCENT_BERNARD_WEBSITE)).div(uint256(100));
+    ongoingBernardFee = ongoingBernardFee.add(partBernardWebsiteFee);
+
+    if (_websiteAddr == address(0)) {
+      websiteFee[OWNER_ADDR] = websiteFee[OWNER_ADDR].add(partBernardWebsiteFee);
+    } else {
+      websiteFee[_websiteAddr] = websiteFee[_websiteAddr].add(partBernardWebsiteFee);
+    }
+  }
+
+  function getSharesNumberAndUpdateOngoingStage(uint256 _amount) private view returns(uint256) {
+    uint16 sharePrice = sharePriceForStage[ongoingStage];
+    uint256 sharesNumberForOngoingStage = msg.value.div(uint256(sharePrice));
+    sharesForAddr[msg.sender] = sharesForAddr[msg.sender].add(sharesNumber);
+
+  }
+  
+}
