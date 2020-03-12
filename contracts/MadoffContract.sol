f741c06cbb211693ebe75aa7fd70b7d3e9421d05
Ivan Solomichev <vanijatko.social@gmail.com>
2020-03-12 12:15:34 +0200

    purchase updated restart game implemented

--- contracts/MadoffContract.sol
+++ contracts/MadoffContract.sol
@@ -21,10 +21,19 @@ contract MadoffContract {
 
   uint16[14] public blocksForStage = [21600, 18000, 14400, 10800, 7200, 3600, 1200, 600, 300, 100, 20, 10, 7, 4];
   uint32[14] public sharesForStageToPurchaseOriginal = [2500, 5000, 3125, 12500, 10000, 62500, 62500, 400000, 390625, 2000000, 1562500, 10000000, 12500000, 25000000];
-  uint32[14] public sharesForStageToPurchase = [2500, 5000, 3125, 12500, 10000, 62500, 62500, 400000, 390625, 2000000, 1562500, 10000000, 12500000, 25000000];
-  uint256[14] public sharePriceForStage = [10000000, 20000000, 40000000, 80000000, 125000000, 160000000, 200000000, 250000000, 320000000, 500000000, 800000000, 1000000000, 1000000000, 1000000000];
+  uint32[14] public sharesForStageToPurchase = [2500,     5000,     3125,     12500,    10000,     62500,     62500,     400000,    390625,    2000000,   562500,    10000000,   12500000,   25000000];
+  uint256[14] public sharePriceForStage =      [10000000, 20000000, 40000000, 80000000, 125000000, 160000000, 200000000, 250000000, 320000000, 500000000, 800000000, 1000000000, 1000000000, 1000000000];
 
-  uint256 public ongoingStageStartBlock;
+    //  2500 * 10000000 -> 1, 0.000000025
+    //  5000 * 20000000 + 3125 * 40000000 -> 3, 0.000000225
+    //  12500 * 80000000 + 10000 * 125000000 + 62500 * 160000000 -> 6, 0.00001225
+    //  62500 * 200000000 + 400000 * 250000000 + 390625 * 320000000 -> 9, 0.0002375
+    //  2000000 * 500000000 + 562500 * 800000000 + 10000000 * 1000000000 + 12500000 * 1000000000 -> 13  0.02395
+    //  24999999 * 1000000000 -> 13 0.024999999
+    //  1 * 1000000000 -> 14, 0.000000001
+    //  25000000 * 1000000000 -> 0.025000000
+    
+  uint256 public latestPurchaseBlock;
   uint256 public ongoingJackpot;
   uint256 public ongoingBernardFee;
   address public ongoingWinner;
@@ -35,7 +44,8 @@ contract MadoffContract {
   event PrizeWithdrawn(address indexed to, uint256 indexed amount);
   event WebsiteFeeWithdrawn(address indexed to, uint256 indexed amount);
   event Purchase(address indexed from, uint256 indexed sharesNumber);
-
+  event GameRestarted();
+  
   constructor(address _OWNER_ADDR) public {
     OWNER_ADDR = _OWNER_ADDR;
   }
@@ -60,13 +70,14 @@ contract MadoffContract {
    * @dev Purchase share(s).
    * @param _websiteAddr Website address, that trx was sent from.
    */
-  function purchase(address _websiteAddr) public payable {
-    if ((ongoingStage == maxStageNumber) && (sharesForStageToPurchase[ongoingStage] == 0)) {
+  function purchase(address _websiteAddr) public payable returns(uint256) {
+    if (latestPurchaseBlock == 0) {
+        latestPurchaseBlock = block.number;
+    } else if (ongoingStage > maxStageNumber) {
+      ongoingStageDurationExceeded();
       resetGameDataToRestart();
-    }
-
-    if (ongoingStageStartBlock.add(blocksForStage[ongoingStage]) > block.number) {
-      ongoingStageDurationEnded();
+    } else if (block.number > latestPurchaseBlock.add(blocksForStage[ongoingStage])) {
+      ongoingStageDurationExceeded();
     }
 
     //  make calculations
@@ -76,38 +87,39 @@ contract MadoffContract {
     uint256 partBernardWebsiteFee = msg.value.mul(uint256(SHARE_PURCHASE_PERCENT_BERNARD_WEBSITE)).div(uint256(100));
     ongoingBernardFee = ongoingBernardFee.add(partBernardWebsiteFee);
 
-    //  TODO: add to all shareholders
-
     if (_websiteAddr == address(0)) {
       websiteFee[OWNER_ADDR] = websiteFee[OWNER_ADDR].add(partBernardWebsiteFee);
     } else {
       websiteFee[_websiteAddr] = websiteFee[_websiteAddr].add(partBernardWebsiteFee);
     }
+    
+    //  TODO: add to all shareholders
 
     uint256 shares = getSharesAndUpdateOngoingStageInfo(msg.value);
     require(shares > 0, "Min 1 share");
     
+    latestPurchaseBlock = block.number;
     ongoingWinner = msg.sender;
     sharesForAddr[ongoingWinner] = sharesForAddr[ongoingWinner].add(shares);
 
     emit Purchase(ongoingWinner, shares);
   }
-
+  
   function resetGameDataToRestart() private {
     ongoingStage = 0;
-    ongoingStageStartBlock = block.number;
     sharesForStageToPurchase = sharesForStageToPurchaseOriginal;
-
-    //  TODO: reset other properties
+    
+    emit GameRestarted();
   }
 
-  function ongoingStageDurationEnded() private {
+  function ongoingStageDurationExceeded() private {
     uint256 jptTmp = ongoingJackpot;
     delete ongoingJackpot;
     prizeForAddr[ongoingWinner] = prizeForAddr[ongoingWinner].add(jptTmp);
     delete ongoingWinner;
-
-    ongoingStageStartBlock = block.number;
+    delete ongoingStage;
+    
+    //  TODO: ongoing shareholders - must not receive new shares. remove to history.
   }
 
   function getSharesAndUpdateOngoingStageInfo(uint256 _amount) private returns(uint256) {
@@ -138,7 +150,6 @@ contract MadoffContract {
 
         delete sharesForStageToPurchase[ongoingStage];
         ongoingStage += 1;
-        ongoingStageStartBlock = block.number;
       }
     } while (loop); 
 
