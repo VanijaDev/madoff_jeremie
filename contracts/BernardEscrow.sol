41c5dc7cf6666517a796255c6805ec23ba8ad401
Ivan Solomichev <vanijatko.social@gmail.com>
2020-03-26 15:44:52 +0200

    BernardEscrow and token implemented. TODO: unit testing

--- contracts/BernardEscrow.sol
+++ contracts/BernardEscrow.sol
@@ -3,10 +3,78 @@ pragma solidity 0.4.25;
 import "./token/MadoffProfitToken.sol";
 import "./SafeMath.sol";
 
+
 contract BernardEscrow {
   using SafeMath for uint256;
 
+  MadoffProfitToken public token;
+
+  uint256 public constant CALCULATION_DISABLED_BLOCKS = 21600;
+  
+  uint256 public prevCalculationBlock;
+  uint256 public tokenFractionProfitCalculatedTimes;
   uint256 public ongoingBernardFee;
 
+  mapping (uint256 => uint256) public tokenFractionProfitForCalculatedIdx;
+  mapping (address => uint256) public profitWithdrawnOnCalculationIdx;
+
+  event BernardFeeWithdrawn(address by, uint256 amount);
+
+  //  MODIFIERS
+  modifier onlyCalculationEnabled() {
+    require(block.number.sub(prevCalculationBlock) >= CALCULATION_DISABLED_BLOCKS, "Calculation disabled");
+    _;
+  }
+
+  modifier onlyTokenOwner() {
+    require(token.balanceOf(msg.sender) > 0, "Not token owner");
+
+    tokenFractionProfitCalculatedTimes = 1;  //  idx 0 can not be used
+    _;
+  }
+
+  modifier onlyToken() {
+    require(msg.sender == address(token), "Not MPT");
+    _;
+  }
+
+
+  constructor (address _token) public {
+    token = MadoffProfitToken(_token);
+  }
+
+  function calculateTokenFractionProfit() public onlyTokenOwner onlyCalculationEnabled {
+    uint256 fractionProfit = ongoingBernardFee.div(10000);
+    require(fractionProfit > 0, "Wrong profit");
+   
+    tokenFractionProfitForCalculatedIdx[tokenFractionProfitCalculatedTimes] = fractionProfit;
+    
+    tokenFractionProfitCalculatedTimes = tokenFractionProfitCalculatedTimes.add(1);
+    prevCalculationBlock = block.number;
+  }
+
+  function withdrawProfit(uint256 _loopLimit) public onlyTokenOwner {
+    _withdrawProfit(msg.sender, _loopLimit);
+  }
+
+  function withdrawProfitFromToken(address recipient, uint256 _loopLimit) public onlyToken {
+    _withdrawProfit(recipient, _loopLimit);
+  }
+
+  function _withdrawProfit(address recipient, uint256 _loopLimit) private {
+    uint256 startIdx = profitWithdrawnOnCalculationIdx[recipient].add(1);
+    require(startIdx < tokenFractionProfitCalculatedTimes, "No profit");
   
+    uint256 endIdx = (tokenFractionProfitCalculatedTimes.sub(startIdx) > _loopLimit) ? startIdx.add(_loopLimit).sub(1) : tokenFractionProfitCalculatedTimes.sub(startIdx);
+    uint256 priceSum;
+
+    for (uint256 i = startIdx; i <= endIdx; i ++) {
+      priceSum = priceSum.add(tokenFractionProfitForCalculatedIdx[i]);
+    }
+    uint256 profit = priceSum.mul(token.balanceOf(recipient));
+
+    profitWithdrawnOnCalculationIdx[recipient] = endIdx;
+    recipient.transfer(profit);
+    emit BernardFeeWithdrawn(recipient, profit);
+  }
 }
--- contracts/token/MadoffProfitToken.sol
+++ contracts/token/MadoffProfitToken.sol
@@ -1,11 +1,97 @@
 pragma solidity 0.4.25;
 
-import "./ERC20.sol";
+import "../SafeMath.sol";
 import "./ERC20Detailed.sol";
+import "../BernardEscrow.sol";
 
-contract MadoffProfitToken is ERC20, ERC20Detailed("MadoffProfitToken", "MPT", 2) {
+contract MadoffProfitToken is ERC20Detailed("MadoffProfitToken", "MPT", 2) {
+
+  using SafeMath for uint256;
+
+  mapping (address => uint256) private _balances;
+
+  uint256 private _totalSupply;
+
+  BernardEscrow escrow;
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+
   constructor (address _owner) public {
-    _mint(_owner, 70);
-    _mint(msg.sender, 30);
+    _mint(_owner, 7000);
+    _mint(msg.sender, 3000);
+  }
+
+  function updateBernardEscrow(address _address) public {
+    require(address(escrow) == 0, "Already set");
+    escrow = BernardEscrow(_address);
+  }
+
+
+  /**
+    * @dev See {IERC20-totalSupply}.
+    */
+  function totalSupply() public view returns (uint256) {
+      return _totalSupply;
+  }
+
+  /**
+    * @dev See {IERC20-balanceOf}.
+    */
+  function balanceOf(address account) public view returns (uint256) {
+      return _balances[account];
+  }
+
+  /**
+    * @dev Transfers token amount.
+    * @param recipient Recipient address.
+    * @param amount Token amount.
+    * @param _loopLimit Limit for loop iteractions.
+    */
+  function transfer(address recipient, uint256 amount, uint256 _loopLimit) public returns (bool) {
+    escrow.withdrawProfitFromToken(msg.sender, _loopLimit);
+    escrow.withdrawProfitFromToken(recipient, _loopLimit);
+
+    _transfer(msg.sender, recipient, amount);
+  }
+
+  /**
+    * @dev Moves tokens `amount` from `sender` to `recipient`.
+    *
+    * This is internal function is equivalent to {transfer}, and can be used to
+    * e.g. implement automatic token fees, slashing mechanisms, etc.
+    *
+    * Emits a {Transfer} event.
+    *
+    * Requirements:
+    *
+    * - `sender` cannot be the zero address.
+    * - `recipient` cannot be the zero address.
+    * - `sender` must have a balance of at least `amount`.
+    */
+  function _transfer(address sender, address recipient, uint256 amount) private {
+      require(sender != address(0), "Transfer from the zero address");
+      require(recipient != address(0), "Transfer to the zero address");
+
+      _balances[sender] = _balances[sender].sub(amount, "Transfer amount exceeds balance");
+      _balances[recipient] = _balances[recipient].add(amount);
+      emit Transfer(sender, recipient, amount);
+  }
+
+  /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+    * the total supply.
+    *
+    * Emits a {Transfer} event with `from` set to the zero address.
+    *
+    * Requirements
+    *
+    * - `to` cannot be the zero address.
+    */
+  function _mint(address account, uint256 amount) private {
+      require(account != address(0), "ERC20: mint to the zero address");
+
+      _totalSupply = _totalSupply.add(amount);
+      _balances[account] = _balances[account].add(amount);
+      emit Transfer(address(0), account, amount);
   }
 }
