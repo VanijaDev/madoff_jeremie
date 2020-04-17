28f0694ade47fd01f2f3db9be33e0c723dc22c21
Ivan Solomichev <vanijatko.social@gmail.com>
2020-04-17 18:30:05 +0300

    ui changes

--- www/dist/assets/js/main.js
+++ www/dist/assets/js/main.js
@@ -33,7 +33,6 @@
     ========================================================================== */
      jQuery('#clock').countdown('2018/10/19',function(event){
       var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
       +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
       +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
