7a91365043655b9420143c68d1af8ba92e4b260e
Ivan Solomichev <vanijatko.social@gmail.com>
2020-04-21 18:19:32 +0300

    tl handling improved

--- www/src/index.js
+++ www/src/index.js
@@ -22,18 +22,18 @@ const Index = {
   currentAccount: "",
 
   setup: function() {
-    if (window.tronWeb.defaultAddress.base58) {
-      this.currentAccount = window.tronWeb.defaultAddress.base58;
-    }
-    console.log("this.currentAccount: ", this.currentAccount);
+    // if (window.tronWeb.defaultAddress.base58) {
+    //   this.currentAccount = window.tronWeb.defaultAddress.base58;
+    // }
+    // console.log("this.currentAccount: ", this.currentAccount);
 
-    if (!this.currentAccount || this.currentAccount.length == 0) {
-      Index.showError(Index.ErrorType.noTronLink);
-    } else if (window.tronWeb.fullNode != 'https://api.trongrid.io' ||
-              window.tronWeb.solidityNode != 'https://api.trongrid.io' ||
-              window.tronWeb.eventServer != 'https://api.trongrid.io') {
-                Index.showError(Index.ErrorType.wrongNode);
-    }
+    // if (!this.currentAccount || this.currentAccount.length == 0) {
+    //   Index.showError(Index.ErrorType.noTronLink);
+    // } else if (window.tronWeb.fullNode != 'https://api.trongrid.io' ||
+    //           window.tronWeb.solidityNode != 'https://api.trongrid.io' ||
+    //           window.tronWeb.eventServer != 'https://api.trongrid.io') {
+    //             Index.showError(Index.ErrorType.wrongNode);
+    // }
   },
 
   setLanguage: function(_langId) {
@@ -54,17 +54,14 @@ const Index = {
 
   /** HELPERS */
   showError: function (_errorType) {
-    console.log("_errorType: ", _errorType);
     let errorText = "";
 
     switch (_errorType) {
       case this.ErrorType.noTronLink:
-        console.log("_errorType: TL", this.ErrorType.noTronLink);
         errorText = "TronLink is not connected. Please, install and log in into TronLink";
       break;
 
       case this.ErrorType.wrongNode:
-        console.log("_errorType: WN", this.ErrorType.wrongNode);
         errorText = "Please, select Main Chain - Mainnet inTronLink";
       break;
     }
@@ -90,20 +87,21 @@ window.onload = function() {
 };
 
 window.addEventListener('message', function (e) {
-  // if (e.data.message && e.data.message.action == "tabReply") {
-  //     console.log("tabReply event", e.data.message)
-  //     if (e.data.message.data.data.node.chain == '_'){
-  //         console.log("tronLink currently selects the main chain")
-  //     }else{
-  //         console.log("tronLink currently selects the side chain")
-  //     }
-  // }
+  if (e.data.message && e.data.message.action == "tabReply") {
+      if (e.data.message.data.data.node.chain == '_'){
+        if (e.data.message.data.node.fullNode == 'https://api.trongrid.io' &&
+            e.data.message.data.node.solidityNode == 'https://api.trongrid.io' &&
+            e.data.message.data.node.eventServer == 'https://api.trongrid.io') {
+              Index.hideError();
+            } else {
+              Index.showError(Index.ErrorType.wrongNode);
+            }
+      }else{
+        Index.showError(Index.ErrorType.wrongNode);
+      }
+  }
 
   if (e.data.message && e.data.message.action == "setAccount") {
-      // console.log("setAccount event e", e)
-      // console.log("setAccount event", e.data.message)
-      // console.log("current address:", e.data.message.data.address)
-
       Index.currentAccount = (e.data.message.data.address) ? window.tronWeb.defaultAddress.base58 : "";
       if (Index.currentAccount.length == 0) {
         Index.showError(Index.ErrorType.noTronLink);
