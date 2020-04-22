fdd432ca0321a539d0ab500c38213f17cf291824
Ivan Solomichev <vanijatko.social@gmail.com>
2020-04-22 19:39:18 +0300

    share price fixed

--- contracts/MadoffContract.sol
+++ contracts/MadoffContract.sol
@@ -20,7 +20,7 @@ contract MadoffContract is CountdownSessionManager, BernardEscrow {
   // uint16[14] public blocksForStage =                    [21600, 18000, 14400, 10800, 7200, 3600, 1200, 600, 300, 100, 20, 10, 7, 4];
   uint32[14] public sharesForStageToPurchaseOriginal =  [2500,     5000,     3125,     12500,    10000,     62500,     62500,     400000,    390625,    2000000,   1562500,   10000000,   12500000,   25000000];
   uint32[14] public sharesForStageToPurchase =          [2500,     5000,     3125,     12500,    10000,     62500,     62500,     400000,    390625,    2000000,   562500,    10000000,   12500000,   25000000];
-  uint256[14] public sharePriceForStage =               [10000000 trx, 20000000 trx, 40000000 trx, 80000000 trx, 125000000 trx, 160000000 trx, 200000000 trx, 250000000 trx, 320000000 trx, 500000000 trx, 800000000 trx, 1000000000 trx, 1000000000 trx, 1000000000 trx];
+  uint256[14] public sharePriceForStage =               [10 trx,   20 trx,   40 trx,   80 trx,   125 trx,   160 trx,   200 trx,   250 trx,   320 trx,   500 trx,   800 trx,   1000 trx,   1000 trx,   1000 trx];
     
   uint256 public latestPurchaseBlock;
   uint256 public ongoingJackpot;
